1d21246b7a04a1b3972a078fb2b8f678e429e814
mahmutcan mutlu <mahmutcanmutlu651@gmail.com>
2025-10-20 20:18:55 +0300

    Add C program to print numbers from 10 to -1
    Write a C program to print numbers from 10 to -1.

---
+++ ornek_15.c
@@ -0,0 +1,8 @@
+//10’dan -1’e kadar sayıları ekrana yazdıran C kodunu yazınız.
+#include <stdio.h>
+int main() {
+    for (int i = 10; i >= -1; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
