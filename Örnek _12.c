33c1d36495a57cb6827b08deb4927fd9c0aee63e
mahmutcan mutlu <mahmutcanmutlu651@gmail.com>
2025-10-20 20:15:00 +0300

    Create Örnek _12.c

---
+++ Örnek _12.c
@@ -0,0 +1,8 @@
+// ekrana 100 den 1 e kadar olan sayilari yazdiran program
+#include <stdio.h>
+int main() {
+    for (int i = 100; i >= 1; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
