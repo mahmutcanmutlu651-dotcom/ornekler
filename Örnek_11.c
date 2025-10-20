64ab53b1cd811ca8babd044992ab08307b72ed6d
mahmutcan mutlu <mahmutcanmutlu651@gmail.com>
2025-10-20 20:14:05 +0300

    Create Örnek_11.c

---
+++ Örnek_11.c
@@ -0,0 +1,30 @@
+// ekrana 1 den 100 e kadar olan sayilari yazdiran program
+#include <stdio.h>  
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+//ekrana 1 den 100 e kadar olan sayilari yazdiran program
+#include <stdio.h>
+int main(){
+    int i=1;
+    while (i<=100)
+    {
+        printf("%d\n",i);
+        i++;        
+    }
+    return 0;
+}
+//ekrana 1 den 100 e kadar olan sayilari yazdiran program
+#include <stdio.h>
+int main(){
+    int i=1;
+    do{
+        printf("%d\n",i);
+        i++;
+    }
+    while (i<=100);
+    return 0;
+}   
