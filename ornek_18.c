8a8f3e33cdf86055fe2222b385e908aae178a9f3
mahmutcan mutlu <mahmutcanmutlu651@gmail.com>
2025-10-26 10:18:01 +0300

    Create ornek_18.c

---
+++ ornek_18.c
@@ -0,0 +1,14 @@
+// break komutu
+
+#include <stdio.h>
+
+int main(){
+    int i;
+    for(i=0;i<=10;i++){
+        if(i==5){
+            break;
+        }
+        printf("%d\n",i);
+    }
+    printf("i'nin son degeri:%d",i);
+}
